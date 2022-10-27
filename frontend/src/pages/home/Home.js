a61ce7caeae96d829cb4df4e7ea422d8535cd336
ugmw96 <ugmw97@gmail.com>
2022-10-27 17:11:36 +0530

    implementing products ui

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from 'react'
 import Card from '../../shared/Card'
 
 const Home = () => {
-    const [data, setData] = useState([]);
-
-    useEffect(()=> {
-      fetch("http://localhost:8080/api/product/all")
-      .then((res) => res.json())
-      .then(data => setData([data]))
-    },[])
-    
+   
   return (
     <div>
-      Home
       <Card title='Gayan' imageUrl='https://picsum.photos/200/300' description='React Router is the most popular routing library in React, but it can be a bit complicated to wrap your head around some of the more complex features. That is why in this article I will be breaking' price={233}/>
     </div>
   )
--- frontend/src/pages/products/Product.js
+++ frontend/src/pages/products/Product.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Card from '../../shared/Card';
 
 const Product = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(()=> {
+    fetch("http://localhost:8080/api/product/all")
+    .then((res) => res.json())
+    .then(data => setData([data]))
+  },[])
+console.log(data);
   return (
     <div>
-      product
+      {data.length > 1 ? data.map((products)=> {
+        return(
+          <Card description={products.name} imageUrl={products.image} price={products.price} title={products.name}/>
+        )
+      }): ''}
     </div>
   )
 }
