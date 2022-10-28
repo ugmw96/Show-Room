b6f98981dee45732f32734028279fbf79c7085f3
ugmw96 <ugmw97@gmail.com>
2022-10-28 17:23:02 +0530

    all product ui implements

--- frontend/src/pages/products/Product.js
+++ frontend/src/pages/products/Product.js
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import Card from '../../shared/Card';
+import Loading from '../../shared/loading/Loading';
+import classes from './Product.module.css';
 
 const Product = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState();
+  const [loading, isLoading] = useState(true);
 
   useEffect(()=> {
-    fetch("http://localhost:8080/api/product/all")
-    .then((res) => res.json())
-    .then(data => setData([data]))
+    const products = async () => {
+      isLoading(true)
+      await fetch("http://localhost:8080/api/product/all")
+      .then((res) => res.json())
+      .then(data => setData(data))
+      isLoading(false);
+    }
+    products();
   },[])
 console.log(data);
   return (
     <div>
-      {data.length > 1 ? data.map((products)=> {
+      {loading && <Loading/>}
+      {!loading && 
+      data.map((products)=> {
         return(
-          <Card description={products.name} imageUrl={products.image} price={products.price} title={products.name}/>
+          <div className={classes.cardSet}>
+            <Card description={products.description} imageUrl={products.image} price={products.price} title={products.name}/>
+          </div>
+          
         )
-      }): ''}
+      })
+      }
     </div>
   )
 }
